b3f082430f46ead996e274c73824a607482d7444
Heber Andrade <hebera75@gmail.com>
2022-03-04 13:36:04 +0100

    Passing Data
    Replacing .data arrow to DATA_FAKE constant
    
    text is returning region.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+
+const FAKE_DATA = [
+    {   id:'data1', value: 10, region: 'North'},
+    { id: 'data2', value: 10, region: 'South' },
+    { id: 'data3', value: 10, region: 'West' },
+    { id: 'data4', value: 10, region: 'East' },
+
+];
+
 d3.select('div')
 //'div'by a tag 
 //'#any-class by a ID
 //'.any-class' by class name
 
     .selectAll('p')
-    .data([1, 2, 3])
+    .data(FAKE_DATA)
     .enter()
     .append('p')
 // How I read this exemple
 // select div all paragraph within  
 // data that was rendered before 
 // enter render the information missing 
-.text(dta => dta);
+.text(dta => dta.region);
 // text method call a function where add data to
-// each element in data row. 
+// each element in data row. https://www.youtube.com/watch?v=TOJ9yjvlapY
