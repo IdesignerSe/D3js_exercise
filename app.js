e4e6a5ffbbd1999e101080b82d584ca6365d8d50
Heber Andrade <hebera75@gmail.com>
2022-03-04 13:19:39 +0100

    Update app.js
    exercise

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log(d3);
+d3.select('div')
+//'div'by a tag 
+//'#any-class by a ID
+//'.any-class' by class name
 
+    .selectAll('p')
+    .data([1, 2, 3])
+    .enter()
+    .append('p')
+// How I read this exemple
+// select div all paragraph within  
+// data that was rendered before 
+// enter render the information missing 
+.text(dta => dta);
+// text method call a function where add data to
+// each element in data row. 
